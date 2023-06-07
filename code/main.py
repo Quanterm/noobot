6dddc8c0bfa674a780ad4cbb334b0228d34feedd
Thanh-Viet Nguyen <topologicrose@protonmail.com>
2023-06-07 11:41:06 +0200

    ordering and backup

--- code/main.py
+++ code/main.py
@@ -10,8 +10,16 @@ import json
 # import sqlite3
 # from flair.data import Sentence
 # from flair.models import TextClassifier
-# Vor Trainierter Text Classifier
-
+# connect to SQLite database
+# conn = sqlite3.connect('chat_database.db')
+# c = conn.cursor()
+# # create messages table (if not exists)
+# c.execute('''CREATE TABLE IF NOT EXISTS messages
+#              (id INTEGER PRIMARY KEY,
+#               content TEXT,
+#               author TEXT,
+#               channel TEXT,
+#               timestamp TEXT)''')
 #Liste aller benötigten Tokens für adressierte Steuerung
 load_dotenv(dotenv_path='./env.env')
 Discord_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -42,7 +50,7 @@ async def on_ready():
     print(f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})')
     achannel = bot.get_channel(int(channel))
-    await achannel.send("Hewwo I am online")
+    await achannel.send("Hello I am ready to Support! UwU")
     
 # Ticketnummer die aufsteigend gezählt wird    
 async def save_ticket_count(count):
@@ -74,55 +82,82 @@ async def support(ctx):
     await save_ticket_count(ticket_number)
 
     def check(msg):
-        # might change to "thread.channel"
         return msg.author == ctx.author and msg.channel == thread
 
     async def wait():
-        user_response = await bot.wait_for('message', check=check, timeout=300)
+        user_response = await bot.wait_for('message', check=check, timeout=600)
         problem = user_response.content.lower()
         return problem
 
-    async def request_support(ctx, thread):
-            role = ctx.guild.get_role(support_id)
-            print(role)
-            if role:
-                for member in role.members:
-                    print(member)
-                    await thread.add_user(member)
-            await thread.send(f"Hey <@&1115553886627971082>! {ctx.author.mention} needs some help!")
-            
-    async def yes_or_no():
-            await thread.send("Please only answer with Yes or No")
+    async def request_support():
+        #    role = ctx.guild.get_role(support_id)
+        #    if role:
+        #        for member in role.members:
+        #            await thread.add_user(member)
+            await thread.send(f"Hey <@&1115553886627971082> ! {ctx.author.mention} needs some help!")
+    
+    async def closing_thread():
+            await thread.send("No response...Closing Ticket. Open a new one if needed!")
+            await thread.edit(archived=True, locked=True)
+         #Keywordbasierte Antworten und Weiterleitung des Problems
     try:
+        problem = ""
         problem = await wait()
-
-        #Keywordbasierte Antworten und Weiterleitung des Problems
 
         if 'internet' in problem:
             await thread.send("I see you are having problems with your internet. Have you tried restarting your router?")
-            await yes_or_no()
+            await thread.send("Please only answer with Yes or No")
             problem = await wait()
             if 'yes' in problem:
-                await thread.send("Sorry i can not help you anymore, i am requesting support!")
-                await request_support(ctx, thread) # TODO FIx
-                
-                
-            else:
+                await thread.send("Sorry i can not help you anymore. I am requesting support!")
+                await request_support()                    
+            elif 'no' in problem:
                 await thread.send("Please restart your router and come back for help if you need any!")
+                problem == None
+                problem = await wait()
+                if problem != None:
+                    await request_support()
+                else:
+                    await closing_thread()  
+            else:
+                await thread.send("You did not answer with yes or no...I will give you one more try")
                 
-        elif 'printer' in problem:
-            await thread.send("Make sure the printer is turned on and properly connected to your computer.")
+        elif 'printer' or 'printing' in problem:
+            await thread.send("I see you are having issues with your printer. Which of those Keywords is most accurate for your problem?")
+            await thread.send("Ink needed\nconnection problems\n")
+            problem = await wait()
+            if 'ink' in problem:
+                await thread.send("Is some kind of ink missing in the printer?")
+                await thread.send("Please only answer with Yes or No")
+                problem = await wait()
+                if 'yes' in problem:
+                    await thread.send("I will request support to order some ink for you")
+                    await request_support()
+                else:
+                    await thread.send("The issue does not seem to be related to ink, i will request some support to help you")
+                    await request_support()
+            elif 'connection' or 'connecting' in problem:
+                await thread.send("If you are having issues connecting to the printer, try both restarting your PC and the printer and come back for help if you need any.")
+                problem == None
+                problem = await wait()
+                if problem != None:
+                    await request_support()
+                else:
+                    await closing_thread()
+            else:
+                await thread.send("It seems like i can not help you here, I am calling for support!")
+                await request_support()
+
         elif 'email' in problem:
             await thread.send("Check your email settings and ensure you have a stable internet connection.")
         elif 'headset' in problem:
             await thread.send("I see you are having problems with your headset. If it is a USB Headset, check if it is connected to your computer or your dockingstation. If it is a bluetooth headset, check if it is connected and charged (sounds of the headset and lights are good indicators!)")
         else:
-            await thread.send("I'm sorry, I'm not sure how to help with that problem. I'll get human help!")
-
-
+            await thread.send("I have not found any keywords, are one of those topics relevant to your problem? Respond with the respective numbers only please")
 
 
     except asyncio.TimeoutError:
         await ctx.send("You took too long to respond...")
 
 bot.run(Discord_TOKEN)
+
